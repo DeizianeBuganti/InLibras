1af01df68ab488c4482a6f1b4a484bf1d98b9956
DeiziBuganti <deizianebuganti@gmail.com>
2016-12-02 19:20:06 -0200

    App InLibras.

---
+++ app/src/main/java/br/edu/pdm/tccfreak/CriancasActivity.java
@@ -0,0 +1,37 @@
+package br.edu.pdm.tccfreak;
+
+
+/*
+*
+* Tela de ambiente para as Crianças Surdas do app InLibras
+*
+* */
+
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CriancasActivity extends AppCompatActivity {
+    // declara o componente webView
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criancas);
+        // amarra a variável com o componente webView
+        mWebView = (WebView) findViewById(R.id.webViewC);
+        // seta as settings do componente
+        WebSettings webSettings = mWebView.getSettings();
+        // habilitado o JavaScript, por se tratar de uma página web
+        webSettings.setJavaScriptEnabled(true);
+        // define a url que será carregada no componente
+        mWebView.loadUrl("http://inlibras.com.br/AndroidCriancas");
+        // e seta o cliente webView, para que o load personalizado abra no componente do app
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+    }
+}
---
+++ app/src/main/java/br/edu/pdm/tccfreak/PaisActivity.java
@@ -0,0 +1,34 @@
+package br.edu.pdm.tccfreak;
+
+
+/*
+*
+* Tela de ambiente para os Pais Ouvintes do app InLibras
+*
+* */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class PaisActivity extends AppCompatActivity {
+    // declara uma variável webView
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pais);
+        // amarra a variável com o componente webView
+        mWebView = (WebView) findViewById(R.id.webViewP);
+        // seta as settings do componente
+        WebSettings webSettings = mWebView.getSettings();
+        // habilitado o JavaScript, por se tratar de uma página web
+        webSettings.setJavaScriptEnabled(true);
+        // define a url que será carregada no componente
+        mWebView.loadUrl("http://inlibras.com.br/AndroidPais");
+        // e seta o cliente webView, para que o load personalizado abra no componente do app
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+    }
+}
