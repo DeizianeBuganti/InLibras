c82a0f8a400bd5512036d4a307071be783cd502c
DeiziBuganti <deizianebuganti@gmail.com>
2016-12-02 19:16:43 -0200

    App InLibras.

---
+++ app/src/main/java/br/edu/pdm/tccfreak/MyAppWebViewClient.java
@@ -0,0 +1,20 @@
+package br.edu.pdm.tccfreak;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Deizi on 01/12/2016.
+ */
+public class MyAppWebViewClient extends WebViewClient {
+
+    // sobrescrevendo o método para load de url do componente webView
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
---
+++ app/src/main/java/br/edu/pdm/tccfreak/SobreActivity.java
@@ -0,0 +1,18 @@
+package br.edu.pdm.tccfreak;
+
+/*
+*
+* Tela Sobre do app InLibras
+*
+* */
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_sobre)
+public class SobreActivity extends AppCompatActivity {
+    // não executa qualquer tipo de operação é somente um elemento visual com informações do sistema
+}
